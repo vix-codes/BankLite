06859aa5f015929c7119e74f853f11ae234b6aaa
Vignesh Muthukumaran <iamvigneshmuthukumaran@gmail.com>
2026-05-26 16:03:59 +0530

    Add simple API test frontend

--- src/main/java/com/example/banklite/controller/BankController.java
+++ src/main/java/com/example/banklite/controller/BankController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin("*")
 public class BankController {
 
     private final BankService bankService;
